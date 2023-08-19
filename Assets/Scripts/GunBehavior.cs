45b38d2c82d0df7f538af9801187c5ce0ebeec77
Duck <87908967+bcomey1@users.noreply.github.com>
2023-08-19 11:42:16 -0700

    ufo hp display

--- Assets/Scripts/GunBehavior.cs
+++ Assets/Scripts/GunBehavior.cs
@@ -7,24 +7,33 @@ public class GunBehavior : MonoBehaviour
     public float range;
     public int power;
     public int reloadTime;
+    private int timer;
 
     private GameObject upgradedObject;
     public GameObject upgradePrefab;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0;
     }
 
     void FixedUpdate()
     {
+        timer++;
+
         var enemies = GameObject.FindGameObjectsWithTag("Enemy");
         var enemyCount = enemies.Length;
         foreach (var enemy in enemies)
         {
         // check distance
             float distanceBetweenObjects = Vector3.Distance(transform.position, enemy.transform.position);
-            if (distanceBetweenObjects <= range) {
-                enemy.GetComponent<UfoBehavior>().hp -= 1;
+            if (distanceBetweenObjects <= range) 
+            {
+                //check timer
+                if (timer >= reloadTime) {
+                    enemy.GetComponent<UfoBehavior>().hp -= 1;
+                    timer = 0;
+                }
             }
             
         }
--- Assets/Scripts/UfoBehavior.cs
+++ Assets/Scripts/UfoBehavior.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UfoBehavior : MonoBehaviour
 {
     public int hp;
     private int speed=3;
+    public GameObject textObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,6 @@ public class UfoBehavior : MonoBehaviour
             gameObject.SetActive(false);
         }
         transform.position-=transform.right*Time.deltaTime*speed;
-
+        textObj.GetComponent<TextMeshPro>().text = hp.ToString();
     }
 }
