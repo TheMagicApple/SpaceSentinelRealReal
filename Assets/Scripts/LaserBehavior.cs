8c97f3d89e664b2da19d0211e6e2dffef7133201
Duck <87908967+bcomey1@users.noreply.github.com>
2023-08-19 16:12:53 -0700

    laser gun final

--- Assets/Scripts/LaserBehavior.cs
+++ Assets/Scripts/LaserBehavior.cs
@@ -6,6 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private LineRenderer lr;
     public int power;
+    public int range;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +16,43 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lr.SetPosition(0, transform.position);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit)) 
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        var enemyCount = enemies.Length;
+        if (enemyCount > 0)
         {
-            if (hit.collider) 
+            var target = closeistTarget(enemies);
+            var distance = Vector3.Distance(target.transform.position, transform.position);
+            if (distance < range)
             {
-                lr.SetPosition(1, hit.point);
-                if (hit.transform.gameObject.tag == "Enemy") {
-                    hit.transform.gameObject.GetComponent<UfoBehavior>().health -= power;
-                }
+                 lr.SetPosition(0, new Vector3(transform.position.x, transform.position.y, transform.position.z));
+                lr.SetPosition(1, new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z));
+                target.gameObject.GetComponent<UfoBehavior>().health -= power;
+            }
+            else
+            {
+
+                transform.rotation = Quaternion.Euler(0, 0, 0);
             }
         }
-        else lr.SetPosition(1, transform.forward*5000);
+
+
+        //else lr.SetPosition(1, transform.forward*5000);
+    }
+
+    public GameObject closeistTarget(GameObject[] enemies)
+    {
+        GameObject closeistTarget = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (GameObject enemy in enemies)
+        {
+            float dist = Vector3.Distance(enemy.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                closeistTarget = enemy;
+                minDist = dist;
+            }
+        }
+        return closeistTarget;
     }
 }
