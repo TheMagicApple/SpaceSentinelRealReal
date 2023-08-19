bb7899ae5e2208de1ad75ab00cc1a1ffc3c0684b
Daniel <90540132+TheMagicApple@users.noreply.github.com>
2023-08-19 10:26:17 -0700

    Changes

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    private bool movingLeft=false;
+    private bool movingRight=false;
+    private bool movingForward=false;
+    private bool movingBackward=false;
+    private int speed=13;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(down("w")) movingForward=true;
+        if(down("a")) movingLeft=true;
+        if(down("s")) movingBackward=true;
+        if(down("d")) movingRight=true;
+        if(up("w")) movingForward=false;
+        if(up("a")) movingLeft=false;
+        if(up("s")) movingBackward=false;
+        if(up("d")) movingRight=false;
+        if(movingLeft) transform.position-=transform.right*Time.deltaTime*speed;
+        if(movingRight) transform.position+=transform.right*Time.deltaTime*speed;
+        if(movingForward) transform.position+=new Vector3(transform.forward.x,0,transform.forward.z)*Time.deltaTime*speed;
+        if(movingBackward) transform.position-=new Vector3(transform.forward.x,0,transform.forward.z)*Time.deltaTime*speed;
+    }
+    bool down(string key){
+        return Input.GetKeyDown(key);
+    }
+    bool up(string key){
+        return Input.GetKeyUp(key);
+    }
+}
---
+++ Assets/Scripts/StarGenerator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarGenerator : MonoBehaviour
+{
+    public GameObject star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<500;i++){
+            float x=Random.Range(-20f, 20f);
+            float z=Random.Range(-20f, 20f);
+            Instantiate(star,new Vector3(x,0,z),Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
