17061a4367a9f749d882d16a446a78075c16cb40
Duck <87908967+bcomey1@users.noreply.github.com>
2023-08-19 17:42:53 -0700

    ufo healthbar final FINAL

--- Assets/Scripts/UfoBehavior.cs
+++ Assets/Scripts/UfoBehavior.cs
@@ -6,11 +6,13 @@ using TMPro;
 public class UfoBehavior : MonoBehaviour
 {
     public int health;
+    private float baseHp;
     private int speed = 3;
     public GameObject healthbar;
     // Start is called before the first frame update
     void Start()
     {
+        baseHp = health;
     }
 
     // Update is called once per frame
@@ -28,7 +30,7 @@ public class UfoBehavior : MonoBehaviour
             //gameObject.SetActive(false);
         }
         if(health>0) transform.position -= transform.right * Time.deltaTime * speed;
-        healthbar.transform.localScale = new Vector3(4.31f*(hp/baseHp), healthbar.transform.localScale.y, healthbar.transform.localScale.z);
+        healthbar.transform.localScale = new Vector3(6f*(health/baseHp), healthbar.transform.localScale.y, healthbar.transform.localScale.z);
 
     }
     IEnumerator die(){
