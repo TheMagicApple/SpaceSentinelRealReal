27813606d48cc93f8a15fa06dd0e8d74504b5c02
potato-potato-potato-potato <jgao666@gmail.com>
2023-08-19 22:03:58 -0700

    e

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public int speed=20;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int speed = 20;
+    private int timetoLive = 250;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position+=transform.forward*Time.deltaTime*speed;   
+        // Decrement the time to live
+        timetoLive--;
+
+        // Check if the time to live has expired
+        if (timetoLive <= 0)
+        {
+            Destroy(gameObject);
+            return; // Exit the method to prevent further movement
+        }
+
+        // Move the bullet
+        transform.position += transform.forward * Time.deltaTime * speed;
     }
-    void OnTriggerEnter(Collider c){
-        if(c.tag=="Enemy"){
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Enemy")
+        {
             Destroy(gameObject);
         }
     }
