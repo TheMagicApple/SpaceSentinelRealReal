1b48fe855812aff7f430b77557aefabbc9bc3f38
potato-potato-potato-potato <jgao666@gmail.com>
2023-08-19 17:03:22 -0700

    e

---
+++ Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public float difficultyScale;
+    public int stage;
+    public GameObject enemy;
+    public static readonly int BASE_ENEMY_HEALTH = 5; // Use 'readonly' and specify the type
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = 1;
+        difficultyScale = 0.5f; // Correct variable name and use 'float'
+    }
+
+    public int enemyHealthscaling(int Stage) // Correct function name
+    {
+        return Mathf.RoundToInt(Stage * difficultyScale); // Use Mathf.RoundToInt
+    }
+
+    public int enemyNumber(int Stage)
+    {
+        return Mathf.RoundToInt((2 * Stage) + 3); // Use Mathf.RoundToInt
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnEnemies()
+    {
+        for (int i = 0; i < enemyNumber(stage); i++)
+        {
+            Instantiate(enemy, new Vector3(53.7f, 0, 0), Quaternion.Euler(0, 0, 0));
+            // set enemy health to enemyHealthscaling(stage)
+            GameObject e = Instantiate(enemy, Vector3.zero, Quaternion.identity);
+            e.GetComponent<UfoBehavior>().health = BASE_ENEMY_HEALTH * enemyHealthscaling(stage);
+        }
+        stage++;
+    }
+}
